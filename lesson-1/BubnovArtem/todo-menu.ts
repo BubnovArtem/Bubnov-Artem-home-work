c7c9dc4a0d5565389260a58ed2d32c43fa7fdce4
Artem Bubnov <bubnovart@yandex.ru>
2016-09-26 23:42:54 +0300

    tag #2

--- lesson-1/BubnovArtem/todo-menu.ts
+++ lesson-1/BubnovArtem/todo-menu.ts
@@ -1,28 +1,65 @@
+type oneElement = {title: string,items?: oneElement[]}
+type menuList = oneElement[];
 
-type oneElement ={title: string,items: menuList}
-type menuList= oneElement[];
-
-let menuList: menuList = [
+let menuList: menuList =  [
     {
-        title: 'JS',
-        items: ['Angular',
-            'React']
+        title: 'Животные',
+        items: [
+            {
+                title: 'Млекопитающие',
+                items: [
+                    {title: 'Коровы'},
+                    {title: 'Ослы'},
+                    {title: 'Собаки'},
+                    {title: 'Тигры'}
+                ],
+            },
+            {
+                title: 'Другие',
+                items: [
+                    {title: 'Змеи'},
+                    {title: 'Птицы'},
+                    {title: 'Ящерицы'},
+                ],
+            },
+        ]
     },
     {
-        title: 'Dart',
-        items: ['Flutter',
-            'Redstone']
-    },
+        title: 'Рыбы',
+        items: [
+            {
+                title: 'Аквариумные',
+                items: [
+                    {title: 'Гуппи'},
+                    {title: 'Скалярии'}
+                ]
+            },
+            {
+                title: 'Форель',
+                items: [
+                    {title: 'Морская форель'}
+                ]
+            },
+        ]
+    }
 ];
 
-function generateMenu(list: menuList): string {
+function generateMenu (list: menuList): string {
     let str: string = `<ul>`;
     for (let a of list) {
-        str += `<li><a class="title">${a.title}</a><ul>`;
-        for (let item of a.items) {
-            str += `<li><a>${item}</a></li>`
+        if (a.title !== undefined && a.items !== undefined)
+        {
+            str += `<li><a class="title">${a.title}</a>`;
         }
-        str += `</li></ul>`
+        else if (a.title !== undefined)
+        {
+            str += `<li><a>${a.title}</a>`;
+        }
+        if (a.items !== undefined)
+        {
+            str += generateMenu(a.items);
+        }
+        str += `</li>`
     }
     str += `</ul>`;
     return str;
@@ -38,4 +75,4 @@ navMenuList.onclick = (ev: MouseEvent) => {
     }
     let parentLi = el.parentNode as HTMLLIElement;
     parentLi.classList.toggle("menu-open")
-}
+};
